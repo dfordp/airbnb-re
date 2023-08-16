8972e8d34a3f0cb79647cdb9da37306f463b71a6
Dilpreet <92905896+dfordp@users.noreply.github.com>
2023-08-16 22:53:16 +0530

    need to rework on navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,8 +1,41 @@
+import Logo from "./Logo"
+import Categories from "./Categories"
+import Container from "../Container"
+import Search from "./Search"
+import UserMenu from "./UserMenu"
+
+interface NavbarProps {
+  currentUser?: SafeUser | null;
+}
+
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+          py-4 
+          border-b-[1px]
+        "
+      >
+        <Container>
+        <div 
+          className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+        >
+          <Logo />
+          <Search />
+          <UserMenu currentUser={currentUser} />
+        </div>
+      </Container>
+      </div>
+      <Categories />
     </div>
   )
 }
